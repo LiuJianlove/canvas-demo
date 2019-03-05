c1291e450158587d1f0081f9eb147e4089647b8a
Stephen <1137657002@qq.com>
2019-03-06 01:19:36 +0800

    触屏设备兼容

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var context = yyy.getContext('2d');
 
 autoSetCanvasSize(yyy)
 
-listenToMouse(yyy)
+listenToUser(yyy)
 
 var eraserEnabled = false
 eraser.onclick = function() {
@@ -50,7 +50,7 @@ function drawLine(x1,y1,x2,y2) {
   context.closePath()
 }
 
-function listenToMouse(canvas){
+function listenToUser(canvas){
   
 
   var using = false
@@ -58,21 +58,61 @@ function listenToMouse(canvas){
     x: undefined,
     y: undefined
 }
-
-canvas.onmousedown = function(aaa) {
-  var x = aaa.clientX
-  var y = aaa.clientY
-  using = true
-  if (eraserEnabled) {
-    context.clearRect(x-5,y-5,10,10)
+//特性检测
+if(document.body.ontouchstart !== undefined){
+  //触屏设备
+  canvas.ontouchstart = function(aaa){
+    var x = aaa.touches[0].clientX
+    var y = aaa.touches[0].clientY
+    using = true
+    if (!using) {return}
+    if(eraserEnabled) {
+        context.clearRect(x-5,y-5,10,10)
+     
+    } else {
+      lastPoint = {
+        "x": x,
+        "y": y
+      }
+    }
+  }
+  canvas.ontouchmove = function(aaa){
+    var x = aaa.touches[0].clientX
+    var y = aaa.touches[0].clientY
     
-  } else {
-    lastPoint = {
-       "x": x,
-       "y": y
-     }
+    if (!using) {return}
+    if(eraserEnabled) {
+        context.clearRect(x-5,y-5,10,10)
+     
+    } else {
+        var newPoint = {
+          "x": x,
+          "y": y
+        }
+        drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
+        lastPoint = newPoint
+      
    }
- }    
+  }
+  canvas.ontouchend = function(){
+    using = false
+  }
+}else{
+  //非触屏设备
+  canvas.onmousedown = function(aaa) {
+    var x = aaa.clientX
+    var y = aaa.clientY
+    using = true
+    if (eraserEnabled) {
+      context.clearRect(x-5,y-5,10,10)
+    
+    } else {
+      lastPoint = {
+        "x": x,
+        "y": y
+      }
+    }
+  }    
  canvas.onmousemove = function(aaa) {
     var x = aaa.clientX
     var y = aaa.clientY
@@ -89,11 +129,13 @@ canvas.onmousedown = function(aaa) {
         drawLine(lastPoint.x,lastPoint.y,newPoint.x,newPoint.y)
         lastPoint = newPoint
       
-   }
+    }
  }
  canvas.onmouseup = function(aaa) {
   using = false
  }
+ }
+
 
 }
 
